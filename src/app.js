36bd150fe33961157dc9681b5c8fef1d3b6b5594
Leilani Titus <leileititus@gmail.com>
2025-12-25 19:40:20 -0500

    Added CORS middleware and test HTML to verify cross-origin requests

--- src/app.js
+++ src/app.js
@@ -4,6 +4,18 @@ const app = express(); // create express app
 
 app.use(express.json());
 
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	if (req.method === 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+		return res.status(200).json({});
+	};
+	next();
+});
+
 // routes import
 import userRouter from './routes/user.route.js';
 import bookRouter from './routes/book.route.js';
